5afa3f430c641231925ed10fb908e27b17eb49f9
Google AI Edge <ai-edge-bot@google.com>
2025-10-20 14:42:42 -0700

    Updated the litert_lm_main to be a simple and self-contained main function to run LLM. Moved the original main function to litert_lm_advanced_main for more advanced usage.
    LiteRT-LM-PiperOrigin-RevId: 821813790

--- runtime/engine/litert_lm_main.cc
+++ runtime/engine/litert_lm_main.cc
@@ -18,13 +18,14 @@
 // 1) Read the corresponding parameters, weight and model file paths.
 // 2) Construct a graph model with the setting.
 // 3) Execute model inference and generate the output.
-//
-// Consider run_llm_inference_engine.sh as an example to run on android device.
 
-#include <optional>
-#include <set>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <sstream>
 #include <string>
-#include <vector>
+#include <utility>
+#include <variant>
 
 #include "absl/base/log_severity.h"  // from @com_google_absl
 #include "absl/flags/flag.h"  // from @com_google_absl
@@ -33,215 +34,130 @@
 #include "absl/log/absl_log.h"  // from @com_google_absl
 #include "absl/log/globals.h"  // from @com_google_absl
 #include "absl/status/status.h"  // from @com_google_absl
-#include "absl/status/statusor.h"  // from @com_google_absl
-#include "absl/strings/numbers.h"  // from @com_google_absl
-#include "absl/strings/str_cat.h"  // from @com_google_absl
+#include "absl/strings/string_view.h"  // from @com_google_absl
+#include "absl/time/time.h"  // from @com_google_absl
+#include "nlohmann/json.hpp"  // from @nlohmann_json
 #include "litert/c/litert_logging.h"  // from @litert
-#include "runtime/engine/litert_lm_lib.h"
+#include "runtime/conversation/conversation.h"
+#include "runtime/conversation/io_types.h"
+#include "runtime/engine/engine.h"
+#include "runtime/engine/engine_settings.h"
+#include "runtime/engine/io_types.h"
+#include "runtime/executor/executor_settings_base.h"
 #include "runtime/util/status_macros.h"
 
 ABSL_FLAG(std::string, backend, "gpu",
           "Executor backend to use for LLM execution (cpu, gpu, etc.)");
-ABSL_FLAG(std::optional<std::string>, vision_backend, std::nullopt,
-          "Backend to use for the vision model (cpu or gpu). If not specified, "
-          "the vision backend will be chosen based on the main backend.");
-ABSL_FLAG(std::optional<std::string>, audio_backend, std::nullopt,
-          "Backend to use for the audio model (cpu or gpu). If not specified, "
-          "the audio backend will be chosen based on the main backend.");
-ABSL_FLAG(std::string, sampler_backend, "",
-          "Sampler backend to use for LLM execution (cpu, gpu, etc.). If "
-          "empty, the sampler backend will be chosen for the best according to "
-          "the main executor, for example, gpu for gpu main executor.");
 ABSL_FLAG(std::string, model_path, "", "Model path to use for LLM execution.");
 ABSL_FLAG(std::string, input_prompt,
           "What is the tallest building in the world?",
           "Input prompt to use for testing LLM execution.");
-ABSL_FLAG(std::string, expected_output, "",
-          "If not empty, the output will be checked against this string. If "
-          "the output does not contain the string, the program will exit with "
-          "an error.");
-ABSL_FLAG(int, max_num_tokens, 0,
-          "Maximum number of tokens or context length to use for LLM execution "
-          "of a graph with dynamic context length. If 0, the maximum context "
-          "length will be determined by some heuristic. On benchmark mode, it "
-          "will be set to one equal to or greater than "
-          "benchmark_prefill_tokens + benchmark_decode_tokens.");
-ABSL_FLAG(std::vector<std::string>, prefill_batch_sizes, {},
-          "A list of maximum numbers of prefill tokens processed at once. If "
-          "empty, it will be the list of one entry with the length of input "
-          "prompt tokens or benchmark_prefill_tokens when benchmark mode is "
-          "enabled.");
-ABSL_FLAG(int, num_output_candidates, 1,
-          "The number of candidates generated for the given prompt, or the "
-          "batch size of the decode signature.");
-ABSL_FLAG(bool, benchmark, false, "Benchmark the LLM execution.");
-ABSL_FLAG(int, benchmark_prefill_tokens, 0,
-          "If benchmark is true and the value is larger than 0, the benchmark "
-          "will use this number to set the number of prefill tokens "
-          "(regardless of the input prompt).");
-ABSL_FLAG(int, benchmark_decode_tokens, 0,
-          "If benchmark is true and the value is larger than 0, the benchmark "
-          "will use this number to set the number of decode steps (regardless "
-          "of the input prompt).");
-ABSL_FLAG(bool, async, true, "Run the LLM execution asynchronously.");
-ABSL_FLAG(bool, report_peak_memory_footprint, false,
-          "Report peak memory footprint.");
-ABSL_FLAG(bool, force_f32, false,
-          "Force float 32 precision for the activation data type.");
-ABSL_FLAG(bool, multi_turns, false,
-          "If true, the command line will ask for multi-turns input.");
-ABSL_FLAG(int, num_cpu_threads, 0,
-          "If greater than 0, the number of CPU threads to use for the LLM "
-          "execution with CPU backend.");
-ABSL_FLAG(bool, gpu_external_tensor_mode, false,
-          "If false (by default), the GPU backend will use no external tensor "
-          "mode which runs slightly faster during decode. It should be set "
-          "true when GPU backend doesn't support no external tensor mode, "
-          "e.g. Vulkan or OpenGL.");
-ABSL_FLAG(bool, configure_magic_numbers, true,
-          "If true and the model contains magic numbers, present magic number "
-          "configs when the model is initialized.");
-ABSL_FLAG(bool, verify_magic_numbers, false,
-          "If true and the model contains magic numbers and test signatures, "
-          "verify magic number configs when the real dimensions that replaced "
-          "magic numbers match with ones of test signatures.");
-ABSL_FLAG(bool, clear_kv_cache_before_prefill, false,
-          "If true, clear kv cache before the first prefill step. This may "
-          "help to disclose any issues related to kv cache.");
-ABSL_FLAG(int, num_logits_to_print_after_decode, 0,
-          "The number of values at the beginning of logits, in the middle of "
-          "logits, and at the end of logits to print after each decode step. "
-          "If 0, disables printing logits.");
-ABSL_FLAG(std::string, score_target_text, "", "Target text to score.");
-ABSL_FLAG(bool, gpu_madvise_original_shared_tensors, true,
-          "If true, the GPU backend will madvise the original shared tensors "
-          "after use.");
-ABSL_FLAG(bool, disable_cache, false, "Disable weight cache.");
+ABSL_FLAG(std::string, input_prompt_file, "", "File path to the input prompt.");
 
 namespace {
 
-// Converts an absl::LogSeverityAtLeast to a LiteRtLogSeverity.
-LiteRtLogSeverity AbslMinLogLevelToLiteRtLogSeverity(
-    absl::LogSeverityAtLeast min_log_level) {
-  int min_log_level_int = static_cast<int>(min_log_level);
-  switch (min_log_level_int) {
-    case -1:
-      // ABSL does not support verbose logging, but passes through -1 as a log
-      // level, which we can use to enable verbose logging in LiteRT.
-      return LITERT_VERBOSE;
-    case static_cast<int>(absl::LogSeverityAtLeast::kInfo):
-      return LITERT_INFO;
-    case static_cast<int>(absl::LogSeverityAtLeast::kWarning):
-      return LITERT_WARNING;
-    case static_cast<int>(absl::LogSeverityAtLeast::kError):
-      return LITERT_ERROR;
-    case static_cast<int>(absl::LogSeverityAtLeast::kFatal):
-      return LITERT_SILENT;
-    default:
-      return LITERT_INFO;
-  }
-}
+using ::litert::lm::Backend;
+using ::litert::lm::Conversation;
+using ::litert::lm::ConversationConfig;
+using ::litert::lm::Engine;
+using ::litert::lm::EngineSettings;
+using ::litert::lm::InputData;
+using ::litert::lm::JsonMessage;
+using ::litert::lm::Message;
+using ::litert::lm::MessageCallbacks;
+using ::litert::lm::ModelAssets;
+using ::nlohmann::json;
 
-absl::StatusOr<std::set<int>> ParsePrefillBatchSizes(
-    const std::vector<std::string>& prefill_batch_sizes) {
-  std::set<int> parsed_prefill_batch_sizes;
-  for (const auto& prefill_batch_size : prefill_batch_sizes) {
-    int size;
-    if (!absl::SimpleAtoi(prefill_batch_size, &size)) {
-      return absl::InvalidArgumentError(
-          absl::StrCat("Invalid prefill batch size: ", prefill_batch_size));
+class PrintMessageCallbacks : public MessageCallbacks {
+ public:
+  explicit PrintMessageCallbacks() = default;
+
+  void OnMessage(const litert::lm::Message& message) override {
+    if (std::holds_alternative<JsonMessage>(message)) {
+      for (const auto& content : std::get<JsonMessage>(message)["content"]) {
+        std::cout << content["text"].get<std::string>();
+      }
+      std::cout << std::flush;
     }
-    parsed_prefill_batch_sizes.insert(size);
   }
-  return parsed_prefill_batch_sizes;
+  void OnComplete() override { std::cout << std::endl << std::flush; }
+  void OnError(const absl::Status& status) override {}
+};
+
+// Gets the input prompt from the command line flag or file.
+std::string GetInputPrompt() {
+  const std::string input_prompt = absl::GetFlag(FLAGS_input_prompt);
+  const std::string input_prompt_file = absl::GetFlag(FLAGS_input_prompt_file);
+  if (!input_prompt.empty() && !input_prompt_file.empty()) {
+    ABSL_LOG(FATAL) << "Only one of --input_prompt and --input_prompt_file can "
+                       "be specified.";
+  }
+  if (!input_prompt.empty()) {
+    return input_prompt;
+  }
+  if (!input_prompt_file.empty()) {
+    std::ifstream file(input_prompt_file);
+    if (!file.is_open()) {
+      std::cerr << "Error: Could not open file " << input_prompt_file
+                << std::endl;
+      return "";
+    }
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    return buffer.str();
+  }
+  // If no input prompt is provided, use the default prompt.
+  return "What is the tallest building in the world?";
 }
 
 absl::Status MainHelper(int argc, char** argv) {
   absl::ParseCommandLine(argc, argv);
-  LiteRtSetMinLoggerSeverity(
-      LiteRtGetDefaultLogger(),
-      AbslMinLogLevelToLiteRtLogSeverity(absl::MinLogLevel()));
+  // Overrides the default for FLAGS_minloglevel to error.
+  absl::SetMinLogLevel(absl::LogSeverityAtLeast::kError);
+  LiteRtSetMinLoggerSeverity(LiteRtGetDefaultLogger(), LITERT_SILENT);
 
-  if (argc <= 1) {
-    ABSL_LOG(INFO)
-        << "Example usage: ./litert_lm_main --model_path=<model_path> "
-           "[--input_prompt=<input_prompt>] "
-           "[--expected_output=<expected_output>] [--backend=<cpu|gpu|npu>] "
-           "[--max_num_tokens=<max_num_tokens>] "
-           "[--prefill_batch_sizes=<size1>[,<size2>,...]]"
-           "[--vision_backend=<cpu|gpu>] [--audio_backend=<cpu|gpu>] "
-           "[--sampler_backend=<cpu|gpu>] [--benchmark] "
-           "[--benchmark_prefill_tokens=<num_prefill_tokens>] "
-           "[--benchmark_decode_tokens=<num_decode_tokens>] "
-           "[--async=<true|false>] [--force_f32=<true|false] "
-           "[--report_peak_memory_footprint] [--multi_turns=<true|false>] "
-           "[--num_cpu_threads=<num_cpu_threads>] "
-           "[--gpu_external_tensor_mode=<true|false>] "
-           "[--configure_magic_numbers=<true|false>] "
-           "[--verify_magic_numbers=<true|false>] "
-           "[--clear_kv_cache_before_prefill=<true|false>] "
-           "[--num_logits_to_print_after_decode=<num_logits_to_print>]"
-           "[--score_target_text=<target_text>]"
-           "[--gpu_madvise_original_shared_tensors=<true|false>]";
-    ABSL_LOG(INFO)
-        << "To provide data for multimodality, use [image:/path/to/image.jpg] "
-           "or [audio:/path/to/audio.wav] in the input prompt. e.g. \"Describe "
-           "the image: [image:/path/to/image.jpg]\", or \"Transcribe the audio "
-           "[audio:/path/to/audio.wav]\"";
-    return absl::InvalidArgumentError("No arguments provided.");
+  const std::string model_path = absl::GetFlag(FLAGS_model_path);
+  if (model_path.empty()) {
+    return absl::InvalidArgumentError("Model path is empty.");
   }
-
-  litert::lm::LiteRtLmSettings settings;
-
-  settings.backend = absl::GetFlag(FLAGS_backend);
-  settings.vision_backend = absl::GetFlag(FLAGS_vision_backend);
-  settings.audio_backend = absl::GetFlag(FLAGS_audio_backend);
-  settings.sampler_backend = absl::GetFlag(FLAGS_sampler_backend);
-  settings.model_path = absl::GetFlag(FLAGS_model_path);
-  settings.input_prompt = absl::GetFlag(FLAGS_input_prompt);
-  settings.expected_output = absl::GetFlag(FLAGS_expected_output);
-  settings.max_num_tokens = absl::GetFlag(FLAGS_max_num_tokens);
+  ASSIGN_OR_RETURN(ModelAssets model_assets,  // NOLINT
+                   ModelAssets::Create(model_path));
+  auto backend_str = absl::GetFlag(FLAGS_backend);
+  ASSIGN_OR_RETURN(Backend backend,
+                   litert::lm::GetBackendFromString(backend_str));
   ASSIGN_OR_RETURN(
-      settings.prefill_batch_sizes,
-      ParsePrefillBatchSizes(absl::GetFlag(FLAGS_prefill_batch_sizes)));
-  settings.num_output_candidates = absl::GetFlag(FLAGS_num_output_candidates);
-  settings.benchmark = absl::GetFlag(FLAGS_benchmark);
-  settings.benchmark_prefill_tokens =
-      absl::GetFlag(FLAGS_benchmark_prefill_tokens);
-  settings.benchmark_decode_tokens =
-      absl::GetFlag(FLAGS_benchmark_decode_tokens);
-  settings.async = absl::GetFlag(FLAGS_async);
-  settings.report_peak_memory_footprint =
-      absl::GetFlag(FLAGS_report_peak_memory_footprint);
-  settings.force_f32 = absl::GetFlag(FLAGS_force_f32);
-  settings.multi_turns = absl::GetFlag(FLAGS_multi_turns);
-  settings.num_cpu_threads = absl::GetFlag(FLAGS_num_cpu_threads);
-  settings.gpu_external_tensor_mode =
-      absl::GetFlag(FLAGS_gpu_external_tensor_mode);
-  settings.configure_magic_numbers =
-      absl::GetFlag(FLAGS_configure_magic_numbers);
-  settings.verify_magic_numbers = absl::GetFlag(FLAGS_verify_magic_numbers);
-  settings.clear_kv_cache_before_prefill =
-      absl::GetFlag(FLAGS_clear_kv_cache_before_prefill);
-  settings.num_logits_to_print_after_decode =
-      absl::GetFlag(FLAGS_num_logits_to_print_after_decode);
-  settings.score_target_text = absl::GetFlag(FLAGS_score_target_text);
-  settings.gpu_madvise_original_shared_tensors =
-      absl::GetFlag(FLAGS_gpu_madvise_original_shared_tensors);
-  settings.disable_cache = absl::GetFlag(FLAGS_disable_cache);
+      EngineSettings engine_settings,
+      EngineSettings::CreateDefault(std::move(model_assets), backend));
+  // Enable benchmark by default.
+  engine_settings.GetMutableBenchmarkParams() =
+      litert::lm::proto::BenchmarkParams();
 
-  // Adjust max_num_tokens and prefill_batch_size if not set on benchmark mode.
-  if (settings.benchmark && settings.benchmark_prefill_tokens > 0) {
-    if (settings.max_num_tokens == 0 && settings.benchmark_decode_tokens > 0) {
-      settings.max_num_tokens =
-          settings.benchmark_prefill_tokens + settings.benchmark_decode_tokens;
-    }
-    if (settings.prefill_batch_sizes.empty()) {
-      settings.prefill_batch_sizes.insert(settings.benchmark_prefill_tokens);
-    }
-  }
+  // Create the engine.
+  ASSIGN_OR_RETURN(auto engine, litert::lm::Engine::CreateEngine(
+                                    std::move(engine_settings)));
 
-  return litert::lm::RunLiteRtLm(settings);
+  // Create the conversation.
+  std::unique_ptr<Conversation> conversation;
+  ASSIGN_OR_RETURN(auto conversation_config,
+                   ConversationConfig::CreateFromSessionConfig(
+                       *engine, litert::lm::SessionConfig::CreateDefault()));
+  ASSIGN_OR_RETURN(conversation,
+                   Conversation::Create(*engine, conversation_config));
+
+  // Prepare the message to send.
+  json content_list = json::array();
+  const std::string input_prompt = GetInputPrompt();
+  std::cout << "input_prompt: " << input_prompt << std::endl;
+  content_list.push_back({{"type", "text"}, {"text", input_prompt}});
+  RETURN_IF_ERROR(conversation->SendMessageAsync(
+      json::object({{"role", "user"}, {"content", content_list}}),
+      std::make_unique<PrintMessageCallbacks>()));
+  RETURN_IF_ERROR(engine->WaitUntilDone(absl::Minutes(10)));
+
+  // Print the benchmark info.
+  auto benchmark_info = conversation->GetBenchmarkInfo();
+  std::cout << std::endl << *benchmark_info << std::endl;
+  return absl::OkStatus();
 }
 
 }  // namespace
