adce86da104a3edf7470d91144f7bf36f578dbc5
Byungchul Kim <byungchul@google.com>
2026-02-11 21:04:59 -0800

    Alloc madvice even with magic numbers
    LiteRT-LM-PiperOrigin-RevId: 869001386

--- runtime/core/engine_advanced_impl.cc
+++ runtime/core/engine_advanced_impl.cc
@@ -77,19 +77,6 @@ absl::StatusOr<Environment&> GetEnvironment(EngineSettings& engine_settings,
                   ->configure_magic_numbers) {
             env_options = helper->GetLiteRtEnvOptions(model_resources,
                                                       main_executor_settings);
-            // Disable madvise original shared tensors for GPU if the model has
-            // magic numbers as it may revert the magic number replacements.
-            if (helper->magic_number_configs() &&
-                helper->magic_number_configs()->num_configs > 0) {
-              auto& executor_settings =
-                  engine_settings.GetMutableMainExecutorSettings();
-              AdvancedSettings new_settings;
-              if (executor_settings.GetAdvancedSettings()) {
-                new_settings = *executor_settings.GetAdvancedSettings();
-              }
-              new_settings.gpu_madvise_original_shared_tensors = false;
-              executor_settings.SetAdvancedSettings(new_settings);
-            }
           }
         } else {
 #if defined(LITERT_DISABLE_NPU)
